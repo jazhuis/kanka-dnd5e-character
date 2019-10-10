15b50546dba35b9c785434bef12708e21f16823d
Jazhuis <jazhuis@gmail.com>
2019-10-10 00:12:15 -0400

    Update AttributeTemplateServiceProvider.php

--- src/AttributeTemplateServiceProvider.php
+++ src/AttributeTemplateServiceProvider.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Kanka\Dnd5eMonster;
+namespace Kanka\Dnd5eCharacter;
 
 use Illuminate\Support\ServiceProvider;
 
@@ -13,29 +13,29 @@ class AttributeTemplateServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        $this->loadViewsFrom(__DIR__.'/../resources/views', 'dnd5emonster');
+        $this->loadViewsFrom(__DIR__.'/../resources/views', 'dnd5echaracter');
 
-        $this->loadTranslationsFrom(realpath(__DIR__.'/../publishable/lang'), 'dnd5emonster');
+        $this->loadTranslationsFrom(realpath(__DIR__.'/../publishable/lang'), 'dnd5echaracter');
 
 
         // Assets
         $this->publishes([
-            __DIR__.'/../publishable/assets' => public_path('vendor/dnd5emonster'),
-        ], 'dnd5emonster');
+            __DIR__.'/../publishable/assets' => public_path('vendor/dnd5echaracter'),
+        ], 'dnd5echaracter');
 
         // Config
         $this->publishes([
-            __DIR__.'/../publishable/config/dnd5emonster.php' => config_path('dnd5emonster.php'),
+            __DIR__.'/../publishable/config/dnd5echaracter.php' => config_path('dnd5echaracter.php'),
         ]);
         $this->mergeConfigFrom(
-            __DIR__.'/../publishable/config/dnd5emonster.php', 'dnd5emonster'
+            __DIR__.'/../publishable/config/dnd5echaracter.php', 'dnd5echaracter'
         );
 
         // Translations
-        $this->loadTranslationsFrom(__DIR__.'/../publishable/lang', 'dnd5emonster');
+        $this->loadTranslationsFrom(__DIR__.'/../publishable/lang', 'dnd5echaracter');
 
         $this->publishes([
-            __DIR__.'/../publishable/lang' => resource_path('lang/vendor/dnd5emonster'),
+            __DIR__.'/../publishable/lang' => resource_path('lang/vendor/dnd5echaracter'),
         ]);
     }
 }
